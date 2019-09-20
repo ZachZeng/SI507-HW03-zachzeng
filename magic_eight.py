be78811967195b97537cf3a6f85df57589ebccae
Zachary Zeng <zachm@Zacharys-MacBook-Pro.local>
2019-09-20 17:23:31 -0400

    add check question function

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,12 @@
 def Ask():
-	name = input("What is your question\n")
-	return name
+	while(1):
+		answer = "";
+		name = input("What is your question\n");
+		if(name == "quit"):
+			break;
+		if(name[len(name)-1] != "?"):
+			answer = "I’m sorry, I can only answer questions.";
+		print(answer);
 
 if __name__ == "__main__":
-	print(Ask());
+	Ask();
